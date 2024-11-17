c17d3e31eb807e0e152b62946ff7d5f524e68c31
Wasif Ali <wasifalicodes@gmail.com>
2024-11-17 18:00:43 +0500

    f

---
+++ js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        $(function () {
+            $('#defaultCountdown').countdown({until: new Date(2025, 0, 7, 8)}); // year, month, date, hour
+        });
+});		
+
